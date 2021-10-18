f61ef24c43f282b2073c5162f592ccc561dbb849
makewheels <makewheels>
2021-10-18 23:43:58 +0800

    init

---
+++ src/main/java/com/example/testkubernetesjavaclient/TestKubernetesJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.testkubernetesjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKubernetesJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKubernetesJavaClientApplication.class, args);
+    }
+
+}
